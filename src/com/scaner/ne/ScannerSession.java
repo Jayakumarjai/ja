ad728c646b48d317566a9fabc1433a7a939111ca
Jayakumar <arunramu0811@gmail.com>
2023-09-14 11:22:21 +0530

    welcome to new practice

---
+++ src/com/scaner/ne/ScannerSession.java
@@ -0,0 +1,31 @@
+package com.scaner.ne;
+
+import java.util.Scanner;
+
+public class ScannerSession {
+	
+	public static void main(String[] args) {
+	
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter value :");
+	String c = s.nextLine();
+	System.out.println("Entered value :"+c);
+	
+	System.out.println("Enter a Value :");
+	int a = s.nextInt();
+	System.out.println("Entered value :"+a);
+	System.out.println("Enter a value :");
+	long b =s.nextLong();
+	System.out.println("Entered value :"+b);
+//	System.out.println("Enter value :");
+//	String c = s.nextLine();
+//	System.out.println("Entered value :"+c);
+	
+	System.out.println("Enter attendance :");
+	boolean d =s.nextBoolean();
+	System.out.println("Entered attendance :"+d);
+	
+	
+	
+	}
+}
